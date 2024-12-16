a111f3192f5decef7060624b6473254bebb1ae81
renukapangamgithub <159953063+renukapangamgithub@users.noreply.github.com>
2024-12-16 18:35:47 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -10,7 +10,7 @@ function Login() {
       email:data.email,
       password:data.password,
     }
-   await axios.post("http://localhost:4001/user/login",userInfo)
+   await axios.post("book-store-app-one-theta.vercel.app/user/login",userInfo)
     .then((res)=>{
       console.log(res.data)
       if(res.data){
