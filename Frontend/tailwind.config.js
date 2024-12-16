a16add54b5739ad91725579474b243cd36ea2701
renukapangamgithub <159953063+renukapangamgithub@users.noreply.github.com>
2024-12-16 19:02:24 +0530

    done

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode:"class",
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    daisyui, // Import daisyui as a module
   ],
   daisyui: {
     themes: ["light"], // Set to light theme explicitly
