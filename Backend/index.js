302f5dcf3e971e4f9af5eb91d2d251a301c452e7
renukapangamgithub <159953063+renukapangamgithub@users.noreply.github.com>
2024-12-16 18:18:23 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,13 @@ import cors from "cors";
 import userRoute from "./route/user.route.js"
 import contactRoute from "./route/contact.route.js"
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app"],
+        methods: ["POST","GET"],
+        credentials: true
+    }
+));
 app.use(express.json())
 
 dotenv.config();
